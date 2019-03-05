5a74b2d8bd5d677acb0d4ddad2d57b25bfd4c25f
Matthew Wilson <automationator@runbox.com>
2019-03-05 12:10:22 -0500

    Whoops

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from pysip import Client
-
-sip_client = Client('localhost:4443', verify=False)
-sip_client.login('mwilson', 'asdfasdfasdf')
-
-print(sip_client.get('indicators/status'))
